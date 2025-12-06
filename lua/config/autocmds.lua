6dcb08444bab848fe9a902522d6375d5f17a04f2
Ricardo A. López Torres <ricardolopez93@gmail.com>
2025-12-06 18:43:55 -0400

    バルイーグル！

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,8 +1,13 @@
--- Autocmds are automatically loaded on the VeryLazy event
--- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
---
--- Add any additional autocmds here
--- with `vim.api.nvim_create_autocmd`
---
--- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
--- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+-- Disable completion + spell for .txt files in LazyVim
+vim.api.nvim_create_autocmd({ "BufRead", "BufNewFile" }, {
+  pattern = "*.txt",
+  callback = function()
+    -- Disable spell checking
+    vim.opt_local.spell = false
+
+    -- Disable LazyVim completion per-buffer
+    vim.b.cmp_enabled = false -- nvim-cmp
+    vim.b.blink_cmp_enabled = false -- blink.cmp
+    vim.b.ai_cmp_enabled = false -- AI completion integration
+  end,
+})
--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,15 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+vim.keymap.del({ "n", "i", "v" }, "<A-j>")
+vim.keymap.del({ "n", "i", "v" }, "<A-k>")
+vim.keymap.del("n", "<C-h>")
+vim.keymap.del("n", "<C-j>")
+vim.keymap.del("n", "<C-k>")
+vim.keymap.del("n", "<C-l>")
+vim.keymap.del("n", "<S-h>")
+vim.keymap.del("n", "<S-l>")
+vim.keymap.del({ "i", "n", "x", "s" }, "<C-s>")
+vim.keymap.del("n", "<leader>-")
+vim.keymap.del("n", "<leader>|")
+vim.keymap.del("n", "<leader>wd")
